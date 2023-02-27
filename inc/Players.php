c895e98a2d368a60b7a6277c8931fe0f147462b4
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-02-27 16:19:26 +0100

    class Players

--- inc/Players.php
+++ inc/Players.php
@@ -1,6 +1,38 @@
 <?php
+    require("Bdd.php");
     $bdd = new Bdd();
     
     class Players {
-        
+        private $id;
+        protected $login;
+        protected $password;
+        protected $coup;
+        protected $score;
+        protected $best_score;
+        protected $ranking;
+        public $conn;
+
+        public function __construct() {
+            global $bdd;
+            $this->conn = $bdd;
+        }
+
+        public function get_login() {
+            return $this->login;
+        }
+
+        public function get_score() {
+            return $this->score;
+        }
+
+        public function get_best_score() {
+            return $this->best_score;
+        }
+
+        public function get_ranking() {
+            return $this->ranking;
+        }
     }
+
+    $player = new Players();
+    var_dump ($player->conn);
